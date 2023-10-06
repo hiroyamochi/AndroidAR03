6beb8796255d1f6544ae663f3dbf228883a670a3
KuMA-official <97272648+KuMA-official@users.noreply.github.com>
2023-10-06 18:23:07 +0900

    complete

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] balloons;
+
+    public Transform rangeA;
+
+    public Transform rangeB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    IEnumerator StartSpawning(){
+        yield return new WaitForSeconds(4);
+
+        for(int i = 0; i < 3; i++){
+            float x = Random.Range(rangeA.position.x, rangeB.position.x);
+            float y = Random.Range(rangeA.position.y, rangeB.position.y);
+            float z = Random.Range(rangeA.position.z, rangeB.position.z);
+            Instantiate(balloons[i], new Vector3(x, y, z), Quaternion.identity);
+        }
+
+        StartCoroutine(StartSpawning());
+    }
+}
